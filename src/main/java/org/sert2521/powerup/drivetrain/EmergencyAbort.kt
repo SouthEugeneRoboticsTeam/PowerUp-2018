1fde4126fb2412b530fc6e094b1c78326b1d4ef2
Andrew Dassonville <dassonville.andrew@gmail.com>
2018-01-28 18:22:02 -0800

    Remove debug printing

--- src/main/java/org/sert2521/powerup/drivetrain/EmergencyAbort.kt
+++ src/main/java/org/sert2521/powerup/drivetrain/EmergencyAbort.kt
@@ -7,11 +7,10 @@ class EmergencyAbort : Command() {
     private var history = listOf(0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F)
 
     override fun execute(): Boolean {
-        history += Drivetrain.ahrs.roll.also { println("raw: $it") }
+        history += Drivetrain.ahrs.roll
         history = history.takeLast(8)
 
         val avg = history.average()
-        println(avg)
         if (avg.absoluteValue >= 12) {
             FixedDrive(0.0).start()
         }
