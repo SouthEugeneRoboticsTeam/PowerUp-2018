7926039633161be6cb80af5b84156e505169fca1
Alex Saveau <saveau.alexandre@gmail.com>
2018-01-29 17:52:05 -0800

    Support customizable control modes from prefs
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- src/main/java/org/sert2521/powerup/util/Oi.kt
+++ src/main/java/org/sert2521/powerup/util/Oi.kt
@@ -1,6 +1,7 @@
 package org.sert2521.powerup.util
 
 import edu.wpi.first.wpilibj.Joystick
+import edu.wpi.first.wpilibj.Preferences
 import edu.wpi.first.wpilibj.XboxController
 
 val leftJoystick = Joystick(LEFT_STICK_PORT)
@@ -9,8 +10,16 @@ val secondaryJoystick = Joystick(SECONDARY_STICK_PORT)
 
 val controller = XboxController(CONTROLLER_PORT)
 
-var controlMode = Control.Controller
+var controlMode
+    get() = Preferences.getInstance().getString("control_mode", null).let {
+        when (it) {
+            "arcade" -> Control.Arcade
+            "controller" -> Control.Controller
+            else -> Control.Tank
+        }
+    }
+    set(value) = Unit // Needed so Kotlin doesn't create a field
 
 enum class Control {
-    Arcade, Tank, Controller
+    Tank, Arcade, Controller
 }
