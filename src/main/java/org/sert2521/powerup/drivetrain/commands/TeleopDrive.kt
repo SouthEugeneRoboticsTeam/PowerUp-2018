4c3c7c922fcda1fc2274654599eb978c72cb4670
Alex Saveau <saveau.alexandre@gmail.com>
2018-01-29 18:05:02 -0800

    Fix inverted arcade drive
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- src/main/java/org/sert2521/powerup/drivetrain/commands/TeleopDrive.kt
+++ src/main/java/org/sert2521/powerup/drivetrain/commands/TeleopDrive.kt
@@ -19,7 +19,7 @@ class TeleopDrive : Command() {
 
     override fun execute(): Boolean {
         when (controlMode) {
-            Control.Arcade -> Drivetrain.arcade(rightJoystick.x, rightJoystick.y)
+            Control.Arcade -> Drivetrain.arcade(rightJoystick.y, rightJoystick.x)
             Control.Tank -> Drivetrain.tank(leftJoystick.y, rightJoystick.y)
             Control.Controller -> Drivetrain.arcade(
                     -controller.getY(GenericHID.Hand.kLeft),
