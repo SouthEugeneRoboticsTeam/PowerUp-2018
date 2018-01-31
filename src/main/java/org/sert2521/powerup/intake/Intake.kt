926dd1fd59d4b1d1b544ef53f9fd77d313a12e5e
Andrew Dassonville <dassonville.andrew@gmail.com>
2018-01-30 18:47:19 -0800

    Update Intake.kt

--- src/main/java/org/sert2521/powerup/intake/Intake.kt
+++ src/main/java/org/sert2521/powerup/intake/Intake.kt
@@ -1,6 +1,6 @@
 package org.sert2521.powerup.intake
 
-import org.sert2521.powerup.intake.commands.TeleopClimber
+import org.sert2521.powerup.intake.commands.TeleopIntake
 import org.sert2521.powerup.util.LEFT_INTAKE_MOTOR
 import org.sert2521.powerup.util.RIGHT_INTAKE_MOTOR
 import org.sertain.command.Subsystem
@@ -15,7 +15,7 @@ object Intake : Subsystem() {
     private val intake =
             Talon(LEFT_INTAKE_MOTOR).autoBreak() + Talon(RIGHT_INTAKE_MOTOR).autoBreak()
 
-    override val defaultCommand = TeleopClimber()
+    override val defaultCommand = TeleopIntake()
 
     fun set(speed: Double) = intake.set(speed)
 
