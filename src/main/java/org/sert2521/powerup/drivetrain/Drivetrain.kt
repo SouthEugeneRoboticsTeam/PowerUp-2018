780bb000337e0f7472aa7542841db13642f76d11
Will <whou125@gmail.com>
2018-01-30 17:53:07 -0800

    Update drivetrain to match development branch

--- src/main/java/org/sert2521/powerup/drivetrain/Drivetrain.kt
+++ src/main/java/org/sert2521/powerup/drivetrain/Drivetrain.kt
@@ -9,37 +9,31 @@ import org.sert2521.powerup.util.LEFT_FRONT_MOTOR
 import org.sert2521.powerup.util.LEFT_REAR_MOTOR
 import org.sert2521.powerup.util.RIGHT_FRONT_MOTOR
 import org.sert2521.powerup.util.RIGHT_REAR_MOTOR
-import org.sertain.RobotLifecycle
 import org.sertain.command.Subsystem
 import org.sertain.hardware.Talon
 import org.sertain.hardware.autoBreak
 import org.sertain.hardware.plus
 import org.sertain.hardware.resetEncoder
 
-object Drivetrain : Subsystem(), RobotLifecycle {
+/**
+ * The robot's primary drive base.
+ */
+object Drivetrain : Subsystem() {
     val ahrs = AHRS(I2C.Port.kMXP)
-
-    val leftDrive =
-            Talon(LEFT_FRONT_MOTOR).autoBreak() + Talon(LEFT_REAR_MOTOR).autoBreak()
-    val rightDrive =
-            Talon(RIGHT_FRONT_MOTOR).autoBreak() + Talon(RIGHT_REAR_MOTOR).autoBreak()
-
-    private val drive = DifferentialDrive(leftDrive, rightDrive)
-
-    override val defaultCommand = TeleopDrive()
 
     val leftSpeed get() = leftDrive.get()
     val rightSpeed get() = leftDrive.get()
 
-    init {
-        leftDrive.autoBreak()
-        rightDrive.autoBreak()
-    }
+    private val leftDrive =
+            Talon(LEFT_FRONT_MOTOR).autoBreak() + Talon(LEFT_REAR_MOTOR).autoBreak()
+    private val rightDrive =
+            Talon(RIGHT_FRONT_MOTOR).autoBreak() + Talon(RIGHT_REAR_MOTOR).autoBreak()
+    private val drive = DifferentialDrive(leftDrive, rightDrive)
+
+    override val defaultCommand = TeleopDrive()
 
     override fun onStart() {
         EmergencyAbort().start()
-
-        stop()
 
         leftDrive.resetEncoder()
         rightDrive.resetEncoder()
