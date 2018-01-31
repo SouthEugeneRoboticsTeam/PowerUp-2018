185864dd51a77e733a2b45d68bef0f9fc71a383c
Will <whou125@gmail.com>
2018-01-30 16:21:37 -0800

    Merge branch 'master' of C:\Users\Will\Desktop\Robotics\PowerUp-2018 with conflicts.

--- src/main/java/org/sert2521/powerup/intake/Intake.kt
+++ src/main/java/org/sert2521/powerup/intake/Intake.kt
@@ -1,5 +1,6 @@
 package org.sert2521.powerup.intake
 
+<<<<<<< Updated upstream
 import org.sert2521.powerup.intake.commands.TeleopIntake
 import org.sert2521.powerup.util.LEFT_INTAKE_MOTOR
 import org.sert2521.powerup.util.RIGHT_INTAKE_MOTOR
@@ -20,4 +21,21 @@ object Intake : Subsystem(), RobotLifecycle {
     fun set(speed: Double) = intake.set(speed)
 
     fun stop() = intake.stopMotor()
+=======
+import org.sert2521.powerup.util.LEFT_INTAKE_MOTOR
+import org.sert2521.powerup.util.RIGHT_INTAKE_MOTOR
+import org.sert2521.powerup.util.leftJoystick
+import org.sertain.command.Command
+import org.sertain.command.Subsystem
+import org.sertain.hardware.Talon
+import org.sertain.hardware.plus
+import org.sertain.hardware.whileActive
+
+val intake: Talon = Talon(LEFT_INTAKE_MOTOR) + Talon(RIGHT_INTAKE_MOTOR)
+
+class Intake: Subsystem() {
+    override fun onCreate() {
+        leftJoystick.whileActive(1, RunIntake(true, intake))
+    }
+>>>>>>> Stashed changes
 }
