26a012a3208708b1de5ec95596e82b59c7a1ed13
Will <whou125@gmail.com>
2018-02-01 17:14:11 -0800

    Add joystick controlled elevator

--- src/main/java/org/sert2521/powerup/elevator/commands/TeleopElevator.kt
+++ src/main/java/org/sert2521/powerup/elevator/commands/TeleopElevator.kt
@@ -1,8 +1,6 @@
 package org.sert2521.powerup.elevator.commands
 
 import org.sert2521.powerup.elevator.Elevator
-import org.sert2521.powerup.util.Control
-import org.sert2521.powerup.util.controlMode
 import org.sert2521.powerup.util.secondaryJoystick
 import org.sertain.command.Command
 
@@ -12,16 +10,8 @@ class TeleopElevator : Command() {
     }
 
     override fun execute(): Boolean {
-        if (controlMode == Control.Arcade || controlMode == Control.Tank) {
-            val speed = 0.5
-            var multiplier = 0
-
-            if (secondaryJoystick.getRawButton(1)) {
-                multiplier = 1
-            } else if (secondaryJoystick.getRawButton(2)) {
-                multiplier = -1
-            }
-            Elevator.set(speed * multiplier)
+        if (secondaryJoystick.trigger) {
+            Elevator.set(secondaryJoystick.y)
         }
         return false
     }
