058a9ff5b8a3d7f135a309822f7eaf157626709f
Alex Saveau <saveau.alexandre@gmail.com>
2018-02-01 17:42:24 -0800

    Update TeleopClimber.kt

--- src/main/java/org/sert2521/powerup/climber/commands/TeleopClimber.kt
+++ src/main/java/org/sert2521/powerup/climber/commands/TeleopClimber.kt
@@ -9,31 +9,13 @@ import org.sert2521.powerup.util.intakeSpeedScalar
 import org.sert2521.powerup.util.rightJoystick
 import org.sertain.command.Command
 
-/**
- * This command allows for teleoperated control of the robot's intake.
- */
 class TeleopClimber : Command() {
     init {
         requires(Intake)
     }
 
     override fun execute(): Boolean {
-        when (controlMode) {
-            Control.Arcade, Control.Tank -> Intake.set(when {
-                // TODO tune defaults
-                rightJoystick.trigger -> intakeSpeedScalar * 1.0
-                rightJoystick.top -> -intakeSpeedScalar * 1.0
-                else -> 0.0
-            })
-            Control.Controller -> {
-                val leftSpeed = controller.getTriggerAxis(GenericHID.Hand.kLeft)
-                val rightSpeed = controller.getTriggerAxis(GenericHID.Hand.kRight)
-
-                // Support variable intake speeds through self-cancellation
-                Climber.set(leftSpeed - rightSpeed)
-            }
-        }
-
+        if (figureoutjoy.getbutton11.ispressed) Climber.set(joy.scaledtrhottle)
         return false
     }
 
