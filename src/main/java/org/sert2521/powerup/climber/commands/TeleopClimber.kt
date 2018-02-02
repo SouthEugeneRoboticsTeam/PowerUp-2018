1231796d6c74041904a05c4a24466bba97668aa0
Alex Saveau <saveau.alexandre@gmail.com>
2018-02-01 17:43:04 -0800

    Update TeleopClimber.kt

--- src/main/java/org/sert2521/powerup/climber/commands/TeleopClimber.kt
+++ src/main/java/org/sert2521/powerup/climber/commands/TeleopClimber.kt
@@ -1,12 +1,5 @@
 package org.sert2521.powerup.climber.commands
 
-import edu.wpi.first.wpilibj.GenericHID
-import org.sert2521.powerup.intake.Intake
-import org.sert2521.powerup.util.Control
-import org.sert2521.powerup.util.controlMode
-import org.sert2521.powerup.util.controller
-import org.sert2521.powerup.util.intakeSpeedScalar
-import org.sert2521.powerup.util.rightJoystick
 import org.sertain.command.Command
 
 class TeleopClimber : Command() {
