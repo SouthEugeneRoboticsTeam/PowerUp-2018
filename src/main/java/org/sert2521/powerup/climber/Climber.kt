3e300564d80f90f9212f8f0f79dcb148838d0590
Alex Saveau <saveau.alexandre@gmail.com>
2018-02-04 11:21:17 -0800

    Update Climber.kt

--- src/main/java/org/sert2521/powerup/climber/Climber.kt
+++ src/main/java/org/sert2521/powerup/climber/Climber.kt
@@ -1,6 +1,6 @@
 package org.sert2521.powerup.climber
 
-import org.sert2521.powerup.climber.commands.TeleopClimber
+import org.sert2521.powerup.climber.commands.Climb
 import org.sert2521.powerup.util.LEFT_CLIMBER_MOTOR
 import org.sert2521.powerup.util.RIGHT_CLIMBER_MOTOR
 import org.sertain.command.Subsystem
@@ -12,9 +12,9 @@ object Climber : Subsystem() {
     private val climber =
             Talon(LEFT_CLIMBER_MOTOR).autoBreak() + Talon(RIGHT_CLIMBER_MOTOR).autoBreak()
 
-    override val defaultCommand = TeleopClimber()
+    override val defaultCommand = Climb()
 
     fun set(speed: Double) = climber.set(speed)
 
-    fun stop() = intake.stopMotor()
+    fun stop() = climber.stopMotor()
 }
