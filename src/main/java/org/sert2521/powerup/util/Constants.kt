6a5ed2deafaf4458cc33d4a157bdd5d12d15ace6
Alex Saveau <saveau.alexandre@gmail.com>
2018-02-05 18:46:22 -0800

    Update Constants.kt

--- src/main/java/org/sert2521/powerup/util/Constants.kt
+++ src/main/java/org/sert2521/powerup/util/Constants.kt
@@ -24,6 +24,6 @@ const val LEFT_CLIMBER_MOTOR = -1
 const val RIGHT_CLIMBER_MOTOR = -1
 
 // Camera
-const val CAMERA_FOV = 60 // Microsoft LifeCam HD 3000 FOV
+const val CAMERA_FOV = 60 // Microsoft LifeCam HD 3000 field of view
 const val CAMERA_WIDTH = 680
 const val DEGREES_PER_PIXEL = CAMERA_FOV / CAMERA_WIDTH
