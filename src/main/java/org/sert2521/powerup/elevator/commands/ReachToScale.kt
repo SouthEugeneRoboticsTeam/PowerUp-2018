2a67a1aa21355eaed559124d9f129453109462c8
Aidan Heller <asfheller@gmail.com>
2018-02-06 20:53:50 -0800

    Create initial reach to scale command

--- src/main/java/org/sert2521/powerup/elevator/commands/ReachToScale.kt
+++ src/main/java/org/sert2521/powerup/elevator/commands/ReachToScale.kt
@@ -4,15 +4,20 @@ import org.sert2521.powerup.elevator.Elevator
 import org.sert2521.powerup.util.secondaryJoystick
 import org.sertain.command.Command
 
-class Elevate : Command() {
+
+class ReachToScale : Command() {
+    private val success: Boolean
+            get() = Elevator.topSwitch1.get() && Elevator.topSwitch2.get()
+    private val abort: Boolean
+            get() = secondaryJoystick.trigger
+
     init {
         requires(Elevator)
     }
 
     override fun execute(): Boolean {
-        Elevator.set(if (secondaryJoystick.trigger) secondaryJoystick.y else 0.0)
-        if (secondaryJoystick.getRawButton(5)) ReachToScale().start()
-        return false
+        Elevator.set(1.0)
+        return success || abort
     }
 
     override fun onDestroy() {
