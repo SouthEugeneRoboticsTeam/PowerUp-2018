ad03e73641feac360ef7073dc9e35eb4b6f0d3c9
Andrew Dassonville <dassonville.andrew@gmail.com>
2018-02-07 16:23:04 -0800

    Fix elevator code

--- src/main/java/org/sert2521/powerup/elevator/Elevator.kt
+++ src/main/java/org/sert2521/powerup/elevator/Elevator.kt
@@ -6,11 +6,12 @@ import org.sert2521.powerup.util.RIGHT_ELEVATOR_MOTOR
 import org.sertain.command.Subsystem
 import org.sertain.hardware.Talon
 import org.sertain.hardware.autoBreak
+import org.sertain.hardware.invert
 import org.sertain.hardware.plus
 
 object Elevator : Subsystem() {
-    private val elevator =
-            Talon(LEFT_ELEVATOR_MOTOR).autoBreak() + Talon(RIGHT_ELEVATOR_MOTOR).autoBreak()
+    private val elevator = Talon(LEFT_ELEVATOR_MOTOR).invert().autoBreak() +
+            Talon(RIGHT_ELEVATOR_MOTOR).autoBreak()
 
     override val defaultCommand = Elevate()
 
