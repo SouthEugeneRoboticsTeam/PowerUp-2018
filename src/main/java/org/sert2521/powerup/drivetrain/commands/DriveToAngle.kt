1c1abb754889de1b16634632e1ae846917542125
Alex Saveau <saveau.alexandre@gmail.com>
2018-02-08 20:49:55 -0800

    Fixes
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

---
+++ src/main/java/org/sert2521/powerup/drivetrain/commands/DriveToAngle.kt
@@ -0,0 +1,31 @@
+package org.sert2521.powerup.drivetrain.commands
+
+import org.sert2521.powerup.drivetrain.Drivetrain
+import org.sertain.command.PidCommand
+import kotlin.math.absoluteValue
+
+class DriveToAngle(private val angle: Double, private val baseSpeed: Double = 0.0) :
+        PidCommand(P, I, D) {
+    private val startAngle = Drivetrain.ahrs.yaw
+
+    init {
+        requires(Drivetrain)
+        setpoint = startAngle + angle
+    }
+
+    override fun execute() =
+            (Drivetrain.ahrs.yaw - startAngle - angle).absoluteValue < ALLOWABLE_ERROR
+
+    override fun returnPidInput() = Drivetrain.ahrs.yaw.toDouble()
+
+    override fun usePidOutput(output: Double) =
+            Drivetrain.drive(baseSpeed + output, baseSpeed - output)
+
+    private companion object {
+        const val P = 1.0
+        const val I = 0.0
+        const val D = 0.0
+
+        const val ALLOWABLE_ERROR = 7.5 // In degrees
+    }
+}
