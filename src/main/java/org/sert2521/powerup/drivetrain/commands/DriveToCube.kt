7aa183fb85bff62f9e0122626b5a1140bb346dec
Jackson Lewis <st.japa6@gmail.com>
2018-02-08 18:08:58 -0800

    Rename NavigateToCube to DriveToCube

--- src/main/java/org/sert2521/powerup/drivetrain/commands/DriveToCube.kt
+++ src/main/java/org/sert2521/powerup/drivetrain/commands/DriveToCube.kt
@@ -5,7 +5,7 @@ import org.sert2521.powerup.drivetrain.Drivetrain
 import org.sertain.command.Command
 import org.sert2521.powerup.util.DEGREES_PER_PIXEL
 
-class NavigateToCube : Command() {
+class DriveToCube : Command() {
     private val table = NetworkTableInstance.getDefault().getTable("Vision")
 
     override fun execute(): Boolean {
