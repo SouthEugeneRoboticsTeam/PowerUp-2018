b8778718cf9e80de3ea1f450cf2a0ad11d65ddcf
Aidan Heller <asfheller@gmail.com>
2018-02-10 18:13:44 -0800

    Set up restrictive logic

--- src/main/java/org/sert2521/powerup/elevator/commands/Elevate.kt
+++ src/main/java/org/sert2521/powerup/elevator/commands/Elevate.kt
@@ -10,13 +10,14 @@ class Elevate : Command() {
     }
 
     override fun execute(): Boolean {
-        val atTop = Elevator.middleTrigger.get() && Elevator.topTrigger.get()
-        val atBottom = Elevator.bottomTrigger.get()
-        if (atTop && secondaryJoystick.y > 0.0)
-        if ((atTop && secondaryJoystick.y > 0.0) || (atBottom && secondaryJoystick.y < 0.0)) {
+        val atTop = !Elevator.middleTrigger.get() && !Elevator.topTrigger.get()
+        val atBottom = !Elevator.bottomTrigger.get()
+
+        if (!(atTop && secondaryJoystick.y > 0.0) && !(atBottom && secondaryJoystick.y < 0.0)) {
             Elevator.set(if (secondaryJoystick.trigger) secondaryJoystick.y else 0.1)
+        } else {
+            Elevator.set(0.1)
         }
-        if (secondaryJoystick.getRawButton(5)) ElevateToScale().start()
         return false
     }
 
