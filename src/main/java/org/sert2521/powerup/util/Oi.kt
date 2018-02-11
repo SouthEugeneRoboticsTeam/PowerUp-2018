efaafcac9bdf465538dc111cf26174afe96a315c
Alex Saveau <saveau.alexandre@gmail.com>
2018-02-11 11:58:41 -0800

    Cleanup
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- src/main/java/org/sert2521/powerup/util/Oi.kt
+++ src/main/java/org/sert2521/powerup/util/Oi.kt
@@ -3,8 +3,6 @@ package org.sert2521.powerup.util
 import edu.wpi.first.wpilibj.Joystick
 import edu.wpi.first.wpilibj.Preferences
 import edu.wpi.first.wpilibj.XboxController
-import org.sert2521.powerup.elevator.commands.SendToScale
-import org.sertain.hardware.whenActive
 
 // Driver joysticks. Left and right used for tank drive, only right used for arcade drive.
 val leftJoystick = Joystick(LEFT_STICK_PORT)
@@ -14,8 +12,6 @@ val rightJoystick = Joystick(RIGHT_STICK_PORT)
 val controller = XboxController(CONTROLLER_PORT)
 
 // Gunner joystick.
-val secondaryJoystick = Joystick(SECONDARY_STICK_PORT).apply {
-    whenActive(5, SendToScale())
-}
+val secondaryJoystick = Joystick(SECONDARY_STICK_PORT)
 
 val intakeSpeedScalar get() = Preferences.getInstance().getDouble("intake_speed_scalar", 0.5)
