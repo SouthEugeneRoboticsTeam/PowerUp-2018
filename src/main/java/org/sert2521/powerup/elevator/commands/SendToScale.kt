912c7086508807256b1559747ffe6eba6d8cd006
Alex Saveau <saveau.alexandre@gmail.com>
2018-02-11 12:29:51 -0800

    Implement correct graph
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- src/main/java/org/sert2521/powerup/elevator/commands/SendToScale.kt
+++ src/main/java/org/sert2521/powerup/elevator/commands/SendToScale.kt
@@ -1,22 +1,20 @@
 package org.sert2521.powerup.elevator.commands
 
 import org.sert2521.powerup.elevator.Elevator
-import org.sert2521.powerup.elevator.Elevator.atTop
-import org.sert2521.powerup.elevator.Elevator.position
 import org.sertain.command.Command
+import java.util.concurrent.TimeUnit
 import kotlin.math.pow
-import kotlin.math.roundToInt
 
-class SendToScale : Command() {
-    private val setPoint = 3330
-
-    fun quadraticCurve(currentPoint: Int): Int {
-        return -((currentPoint - setPoint).toDouble().pow(2.0)).roundToInt() //* (currentPoint - setPoint)
+class SendToScale : Command(5, TimeUnit.SECONDS) {
+    override fun execute(): Boolean {
+        // See https://www.desmos.com/calculator/l3pczqcg88
+        Elevator.set(GRADIENT.pow(1000 / (TARGET_POSITION - Elevator.position)) - MAX_POWER)
+        return Elevator.atTop
     }
 
-    override fun execute(): Boolean {
-        Elevator.set(quadraticCurve(position).toDouble())
-        println("Do dabbity dough")
-        return atTop || position >= 3330
+    private companion object {
+        const val GRADIENT = 0.85
+        const val MAX_POWER = 0.5
+        const val TARGET_POSITION = 3333
     }
 }
