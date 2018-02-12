dec4a77621a8bccc5a342892a94379d550d8a117
Alex Saveau <saveau.alexandre@gmail.com>
2018-02-11 21:28:19 -0800

    Fix flipped encoder and tune elevator target graphs
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- src/main/java/org/sert2521/powerup/elevator/commands/SendToTarget.kt
+++ src/main/java/org/sert2521/powerup/elevator/commands/SendToTarget.kt
@@ -4,13 +4,18 @@ import org.sert2521.powerup.elevator.Elevator
 import org.sertain.command.Command
 import java.util.concurrent.TimeUnit
 import kotlin.math.pow
+import kotlin.properties.Delegates
 
 abstract class SendToTarget(private val target: Int) : Command(5, TimeUnit.SECONDS) {
     protected abstract val isAtTarget: Boolean
-    private val wasBelowTarget by lazy { target <= Elevator.position }
+    private var wasBelowTarget: Boolean by Delegates.notNull()
 
     init {
         requires(Elevator)
+    }
+
+    override fun onCreate() {
+        wasBelowTarget = Elevator.position <= target
     }
 
     override fun execute(): Boolean {
@@ -18,17 +23,21 @@ abstract class SendToTarget(private val target: Int) : Command(5, TimeUnit.SECON
         val maxPower = if (wasBelowTarget) 1 - MAX_UP_POWER else 1 + MAX_DOWN_POWER
 
         // Example: https://www.desmos.com/calculator/l3pczqcg88
-        Elevator.set(gradient.pow(1000 / (target - Elevator.position)) - maxPower)
+        Elevator.set(if (wasBelowTarget) {
+            gradient.pow(1000 / (target - Elevator.position)) - maxPower + (1 - gradient)
+        } else {
+            gradient.pow(1000 / Elevator.position) - maxPower
+        })
 
         return isAtTarget
     }
 
     private companion object {
-        const val UP_GRADIENT = 0.85
-        const val DOWN_GRADIENT = 1.15
+        const val UP_GRADIENT = 0.8
+        const val DOWN_GRADIENT = 1.2
 
-        const val MAX_UP_POWER = 0.7
-        const val MAX_DOWN_POWER = 0.3
+        const val MAX_UP_POWER = 0.85
+        const val MAX_DOWN_POWER = 0.2
     }
 }
 
