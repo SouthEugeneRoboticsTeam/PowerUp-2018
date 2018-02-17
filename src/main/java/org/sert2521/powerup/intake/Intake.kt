a356f3c487eafb71da09fe97c437f35289de0c77
Andrew Dassonville <dassonville.andrew@gmail.com>
2018-02-17 15:54:30 -0800

    Add intake trigger

--- src/main/java/org/sert2521/powerup/intake/Intake.kt
+++ src/main/java/org/sert2521/powerup/intake/Intake.kt
@@ -3,9 +3,11 @@ package org.sert2521.powerup.intake
 import edu.wpi.first.wpilibj.Spark
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import org.sert2521.powerup.intake.commands.TeleopIntake
+import org.sert2521.powerup.util.INTAKE_TRIGGER_PORT
 import org.sert2521.powerup.util.LEFT_INTAKE_MOTOR
 import org.sert2521.powerup.util.RIGHT_INTAKE_MOTOR
 import org.sertain.command.Subsystem
+import org.sertain.hardware.DigitalInput
 import org.sertain.hardware.invert
 
 /**
@@ -15,7 +17,13 @@ object Intake : Subsystem() {
     private val left = Spark(LEFT_INTAKE_MOTOR).invert()
     private val right = Spark(RIGHT_INTAKE_MOTOR)
 
+    private val intakeTrigger = DigitalInput(INTAKE_TRIGGER_PORT).invert()
+
     override val defaultCommand = TeleopIntake()
+
+    override fun execute() {
+        SmartDashboard.putData("Intake Trigger", intakeTrigger)
+    }
 
     fun set(speed: Double) {
         SmartDashboard.putNumber("Intake Speed", speed)
