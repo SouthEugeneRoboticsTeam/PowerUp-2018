fdc9c059c8eab5f18211aee401a2bdeb64a523a9
Alex Saveau <saveau.alexandre@gmail.com>
2018-02-17 15:44:23 -0800

    Scale elevator down speed
    Co-authored-by: Andrew Dassonville <dassonville.andrew@gmail.com>

--- src/main/java/org/sert2521/powerup/elevator/commands/Elevate.kt
+++ src/main/java/org/sert2521/powerup/elevator/commands/Elevate.kt
@@ -3,6 +3,7 @@ package org.sert2521.powerup.elevator.commands
 import org.sert2521.powerup.elevator.Elevator
 import org.sert2521.powerup.util.secondaryJoystick
 import org.sertain.command.Command
+import kotlin.math.sign
 
 class Elevate : Command() {
     init {
@@ -14,7 +15,7 @@ class Elevate : Command() {
         val isNotLeavingExtremities =
                 (!Elevator.atTop || y < 0.0) && (!Elevator.atBottom || y > 0.0)
         Elevator.set(if (secondaryJoystick.trigger && isNotLeavingExtremities) {
-            y
+            if (y.sign < 0) DOWN_SPEED_SCALAR * y else y
         } else {
             0.1
         })
@@ -23,4 +24,8 @@ class Elevate : Command() {
     }
 
     override fun onDestroy() = Elevator.stop()
+
+    private companion object {
+        const val DOWN_SPEED_SCALAR = 0.6
+    }
 }
