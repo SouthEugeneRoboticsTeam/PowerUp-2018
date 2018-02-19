56f8552255ed1bca4ddbbc8f2cb83f30a8451616
Alex Saveau <saveau.alexandre@gmail.com>
2018-02-18 18:08:09 -0800

    Invert right intake motor
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- src/main/java/org/sert2521/powerup/intake/Intake.kt
+++ src/main/java/org/sert2521/powerup/intake/Intake.kt
@@ -15,7 +15,7 @@ import org.sertain.hardware.invert
  */
 object Intake : Subsystem() {
     private val left = Spark(LEFT_INTAKE_MOTOR).invert()
-    private val right = Spark(RIGHT_INTAKE_MOTOR)
+    private val right = Spark(RIGHT_INTAKE_MOTOR).invert()
 
     private val intakeTrigger = DigitalInput(INTAKE_TRIGGER_PORT).invert()
 
