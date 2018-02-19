80dd35c54c171b6f0f36d7e36150429bf9077e79
Jackson Lewis <st.japa6@gmail.com>
2018-02-18 16:33:22 -0800

    Replace Drivetrain.tank with DriveToAngle command

--- src/main/java/org/sert2521/powerup/drivetrain/commands/DriveToCube.kt
+++ src/main/java/org/sert2521/powerup/drivetrain/commands/DriveToCube.kt
@@ -4,6 +4,7 @@ import edu.wpi.first.networktables.NetworkTableInstance
 import org.sert2521.powerup.drivetrain.Drivetrain
 import org.sertain.command.Command
 import org.sert2521.powerup.util.DEGREES_PER_PIXEL
+import org.sert2521.powerup.drivetrain.commands.DriveToAngle
 
 class DriveToCube : Command() {
     private val table = NetworkTableInstance.getDefault().getTable("Vision")
@@ -12,15 +13,13 @@ class DriveToCube : Command() {
         if (!table.getEntry("cube_found").getBoolean(false)) return true
 
         val degrees = table.getEntry("cube_offset_x").getDouble(0.0) * DEGREES_PER_PIXEL
-        val turn = degrees * TURN_MODIFIER
 
-        Drivetrain.tank(BASE_SPEED - turn, BASE_SPEED + turn)
+        DriveToAngle(degrees, BASE_SPEED).start()
 
         return false
     }
 
     private companion object {
         val BASE_SPEED = 0.25
-        val TURN_MODIFIER = 0.01
     }
 }
