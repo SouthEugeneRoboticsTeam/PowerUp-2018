9294bc7763266f04d5175849b7b1fab075e93368
Jackson Lewis <st.japa6@gmail.com>
2018-02-18 16:26:25 -0800

    Replace Drivetrain.tank with DriveToAngle command

--- src/main/java/org/sert2521/powerup/drivetrain/commands/DriveToCube.kt
+++ src/main/java/org/sert2521/powerup/drivetrain/commands/DriveToCube.kt
@@ -12,15 +12,13 @@ class DriveToCube : Command() {
         if (!table.getEntry("cube_found").getBoolean(false)) return true
 
         val degrees = table.getEntry("cube_offset_x").getDouble(0.0) * DEGREES_PER_PIXEL
-        val turn = degrees * TURN_MODIFIER
 
-        Drivetrain.tank(BASE_SPEED - turn, BASE_SPEED + turn)
+        DriveToAngle(degrees, BASE_SPEED).start()
 
         return false
     }
 
     private companion object {
         val BASE_SPEED = 0.25
-        val TURN_MODIFIER = 0.01
     }
 }
