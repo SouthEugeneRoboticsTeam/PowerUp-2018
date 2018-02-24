934ba762d210061320e63070b043308f492074b3
Alex Saveau <saveau.alexandre@gmail.com>
2018-02-23 19:52:46 -0800

    Update Camera.kt

--- src/main/java/org/sert2521/powerup/util/Camera.kt
+++ src/main/java/org/sert2521/powerup/util/Camera.kt
@@ -52,7 +52,17 @@ object Camera {
                     val text = "Time: ${DriverStation.getInstance().matchTime}\nMode: $gameMode"
                     Imgproc.putText(source, text, Point(0.0, 0.0), FONT_HERSHEY_SIMPLEX, 10.0, Scalar(0.0, 255.0, 0.0), 2)
                     Imgproc.rectangle(source, Point(5.0, 5.0), Point(165.0, 60.0), allianceColor, -1)
-                    Imgproc.putText(source, "${DriverStation.getInstance().alliance}", Point(5.0, 53.0), FONT_HERSHEY_COMPLEX, 1.85, Scalar(255.0, 255.0, 255.0), 2, 16, false)
+                    Imgproc.putText(
+                            source,
+                            DriverStation.getInstance().alliance.toString(),
+                            Point(5.0, 53.0),
+                            FONT_HERSHEY_COMPLEX,
+                            1.85,
+                            Scalar(255.0, 255.0, 255.0),
+                            2,
+                            16,
+                            false
+                    )
 
                     out.write(source)
                     outputStream.putFrame(source)
