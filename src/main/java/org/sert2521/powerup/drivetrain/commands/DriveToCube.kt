ed0d7b099b1109adbe8d4cca3bdacb8463d2d88b
Alex Saveau <saveau.alexandre@gmail.com>
2018-03-03 18:50:38 -0800

    Fix vision cube finder stopping prematurely
    Regression introduced while optimizing in 3ad65a7d989eefc5a4ec76113043acb374a4a119

--- src/main/java/org/sert2521/powerup/drivetrain/commands/DriveToCube.kt
+++ src/main/java/org/sert2521/powerup/drivetrain/commands/DriveToCube.kt
@@ -3,6 +3,7 @@ package org.sert2521.powerup.drivetrain.commands
 import edu.wpi.first.networktables.NetworkTable
 import edu.wpi.first.networktables.NetworkTableInstance
 import org.sert2521.powerup.drivetrain.Drivetrain
+import org.sert2521.powerup.elevator.Elevator
 import org.sert2521.powerup.intake.Intake
 import org.sert2521.powerup.util.DEGREES_PER_PIXEL
 
@@ -12,7 +13,7 @@ class DriveToCube : AngleDriver(0.01) {
     override fun execute(output: Double): Boolean {
         Drivetrain.drive(BASE_SPEED + output, BASE_SPEED - output)
         updateSetpoint(table.getEntry("cube_offset_x").getDouble(0.0) * DEGREES_PER_PIXEL)
-        return Intake.hasCube
+        return Intake.hasCube && Elevator.atBottom
     }
 
     private fun updateSetpoint(offset: Double) {
