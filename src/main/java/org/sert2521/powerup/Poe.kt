419d2dbbbfcbc38a93b2923ef95b00c77f36bef5
Andrew Dassonville <dassonville.andrew@gmail.com>
2018-03-04 17:42:40 -0800

    Remove unused crap

--- src/main/java/org/sert2521/powerup/Poe.kt
+++ src/main/java/org/sert2521/powerup/Poe.kt
@@ -1,14 +1,11 @@
 package org.sert2521.powerup
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
-import openrio.powerup.MatchData
 import org.sert2521.powerup.autonomous.Auto
 import org.sert2521.powerup.climber.Climber
 import org.sert2521.powerup.drivetrain.Drivetrain
 import org.sert2521.powerup.elevator.Elevator
 import org.sert2521.powerup.intake.Intake
 import org.sert2521.powerup.util.Modes
-import org.sert2521.powerup.util.calculateAutoMode
 import org.sertain.Robot
 
 class Poe : Robot() {
@@ -19,9 +16,5 @@ class Poe : Robot() {
         Climber
         Auto
         Modes
-    }
-
-    override fun executeDisabled() {
-
     }
 }
