f0036577b535515ef7be9b3591e4286ff4930de5
Alex Saveau <saveau.alexandre@gmail.com>
2018-03-10 15:25:48 -0800

    More drive straight tuning
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- src/main/java/org/sert2521/powerup/drivetrain/commands/DriveStraight.kt
+++ src/main/java/org/sert2521/powerup/drivetrain/commands/DriveStraight.kt
@@ -5,7 +5,7 @@ import org.sertain.command.Command
 
 class DriveStraight(private val speed: Double, timeoutMillis: Long) : Command(timeoutMillis) {
     override fun execute(): Boolean {
-        Drivetrain.drive(speed, speed * 0.9)
+        Drivetrain.drive(speed, speed * 0.95)
         return false
     }
 }
