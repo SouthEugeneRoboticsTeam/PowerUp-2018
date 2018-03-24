1fa0233c9f5ddcd3edb1d9bf0defa266853cb540
Aidan Heller <asfheller@gmail.com>
2018-03-24 14:21:07 -0700

    Change turn to angle to use encoders (#54)
    Co-authored-by: Andrew Dassonville <dassonville.andrew@gmail.com>
    Co-authored-by: Alex Saveau <saveau.alexandre@gmail.com>

--- src/main/java/org/sert2521/powerup/drivetrain/commands/TurnToAngle.kt
+++ src/main/java/org/sert2521/powerup/drivetrain/commands/TurnToAngle.kt
@@ -1,21 +1,39 @@
 package org.sert2521.powerup.drivetrain.commands
 
 import org.sert2521.powerup.drivetrain.Drivetrain
+import org.sert2521.powerup.util.ENCODER_TICKS_PER_REVOLUTION
+import org.sert2521.powerup.util.WHEELBASE_WIDTH
+import org.sert2521.powerup.util.WHEEL_DIAMETER
+import org.sertain.command.Command
 import kotlin.math.absoluteValue
 
-class TurnToAngle(private val angle: Double) : AngleDriver(1.0) {
-    private val startAngle by lazy { Drivetrain.ahrs.yaw }
+class TurnToAngle(angle: Double) : Command() {
+    private val startLeft by lazy { Drivetrain.leftPosition }
+    private val startRight by lazy { Drivetrain.rightPosition }
 
-    override fun onCreate() {
-        setpoint = startAngle + angle
+    private val turnAmount = angle / 360 * FULL_TURN // In encoder ticks
+
+    init {
+        requires(Drivetrain)
     }
 
-    override fun execute(output: Double): Boolean {
-        Drivetrain.drive(output, -output)
-        return (Drivetrain.ahrs.yaw - startAngle - angle).absoluteValue < ALLOWABLE_ERROR
+    override fun execute(): Boolean {
+        val leftDelta = Drivetrain.leftPosition - startLeft
+        val rightDelta = startRight - Drivetrain.rightPosition
+
+        val leftError = turnAmount - leftDelta
+        val rightError = turnAmount - rightDelta
+
+        Drivetrain.drive(SPEED_FACTOR * leftError, -SPEED_FACTOR * rightError)
+
+        return leftError.absoluteValue < ALLOWABLE_ERROR
+                || rightError.absoluteValue < ALLOWABLE_ERROR
     }
 
     private companion object {
-        const val ALLOWABLE_ERROR = 5.0 // In degrees
+        const val ALLOWABLE_ERROR = 100 // Encoder ticks
+        const val FULL_TURN = WHEELBASE_WIDTH / WHEEL_DIAMETER * ENCODER_TICKS_PER_REVOLUTION
+
+        const val SPEED_FACTOR = 0.45 * 0.00014
     }
 }
