6330f408a564ea85e6bf055b8712e8fa37d858e1
Andrew Dassonville <dassonville.andrew@gmail.com>
2018-03-28 18:17:41 -0700

    Fix

--- src/main/java/org/sert2521/powerup/drivetrain/commands/TurnToAngle.kt
+++ src/main/java/org/sert2521/powerup/drivetrain/commands/TurnToAngle.kt
@@ -6,18 +6,29 @@ import org.sert2521.powerup.util.WHEELBASE_WIDTH
 import org.sert2521.powerup.util.WHEEL_DIAMETER
 import org.sertain.command.PidCommand
 import kotlin.math.absoluteValue
+import kotlin.math.sign
+import kotlin.properties.Delegates
 
-class TurnToAngle(private val angle: Double) : PidCommand(p = 0.00008, d = 0.00003) {
+class TurnToAngle(private val angle: Double) : PidCommand(p = 0.0000725, d = 0.00007) {
+    private var leftStart: Int by Delegates.notNull()
+    private var rightStart: Int by Delegates.notNull()
+
+    private val position
+        get() = angle.sign * Drivetrain.run {
+            (leftStart - leftPosition).absoluteValue + (rightStart - rightPosition).absoluteValue
+        } / 2
+
+    private val error get() = setpoint - position
+
     init {
         requires(Drivetrain)
     }
 
-    private val encoderValue get() = Drivetrain.leftPosition
-
-    private val error get() = setpoint - encoderValue
-
     override fun onCreate() {
-        setpoint = encoderValue + (angle / 360 * FULL_TURN)
+        leftStart = Drivetrain.leftPosition
+        rightStart = Drivetrain.rightPosition
+
+        setpoint = angle / 360 * FULL_TURN
     }
 
     override fun execute(output: Double): Boolean {
@@ -25,12 +36,10 @@ class TurnToAngle(private val angle: Double) : PidCommand(p = 0.00008, d = 0.000
         return error.absoluteValue < ALLOWABLE_ERROR
     }
 
-    override fun returnPidInput(): Double {
-        return Drivetrain.leftPosition.toDouble()
-    }
+    override fun returnPidInput() = position
 
     private companion object {
-        const val ALLOWABLE_ERROR = 750.0
+        const val ALLOWABLE_ERROR = 400.0
         const val FULL_TURN = WHEELBASE_WIDTH / WHEEL_DIAMETER * ENCODER_TICKS_PER_REVOLUTION
     }
 }
