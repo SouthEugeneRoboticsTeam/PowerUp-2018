946427880b9e3cd5cbffa2eeef3597618f59c80e
Alex Saveau <saveau.alexandre@gmail.com>
2018-03-31 11:08:14 -0700

    Even more auto path tuning

--- src/main/java/org/sert2521/powerup/autonomous/Paths.kt
+++ src/main/java/org/sert2521/powerup/autonomous/Paths.kt
@@ -201,7 +201,7 @@ object LeftToLeftScalePath : PathBase() {
     override var points = arrayOf(
             0.0 with 3.0 angle 0.0,
             4.2 with 3.0 angle 0.0,
-            6.6 with 2.1 angle -30.0
+            6.5 with 2.1 angle -30.0
     )
 }
 
@@ -209,7 +209,7 @@ object RightToRightScalePath : PathBase() {
     override var points = arrayOf(
             0.0 with -3.0 angle 0.0,
             4.2 with -3.0 angle 0.0,
-            6.6 with -2.6 angle 30.0
+            6.6 with -2.5 angle 30.0
     )
 }
 
