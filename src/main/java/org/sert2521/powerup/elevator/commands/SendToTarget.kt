aa6c4a574501742a1176143365c28eda8bd3298c
Alex Saveau <saveau.alexandre@gmail.com>
2018-04-06 17:39:21 -0700

    Hardcode send to target speeds since the elevator breaker keeps tripping

--- src/main/java/org/sert2521/powerup/elevator/commands/SendToTarget.kt
+++ src/main/java/org/sert2521/powerup/elevator/commands/SendToTarget.kt
@@ -5,29 +5,18 @@ import org.sertain.command.Command
 import kotlin.math.pow
 import kotlin.properties.Delegates
 
-abstract class SendToTarget(private val target: Int, requireSubsystem: Boolean) : Command(5000) {
+abstract class SendToTarget(requireSubsystem: Boolean) : Command(5000) {
     protected abstract val isAtTarget: Boolean
-    private var wasBelowTarget: Boolean by Delegates.notNull()
+    protected val downSpeed = {
+        (DOWN_GRADIENT.pow(1000 / Elevator.position.coerceAtLeast(1)) - MAX_DOWN_POWER)
+                .coerceAtMost(MIN_DOWN_POWER)
+    }
 
     init {
         if (requireSubsystem) requires(Elevator)
     }
 
-    override fun onCreate() {
-        wasBelowTarget = Elevator.position <= target
-    }
-
-    override fun execute(): Boolean {
-        // Example: https://www.desmos.com/calculator/l3pczqcg88
-        Elevator.set(if (wasBelowTarget) {
-            1.0
-        } else {
-            (DOWN_GRADIENT.pow(1000 / Elevator.position.coerceAtLeast(1)) - MAX_DOWN_POWER)
-                    .coerceAtMost(MIN_DOWN_POWER)
-        })
-
-        return isAtTarget
-    }
+    override fun execute() = isAtTarget
 
     override fun onDestroy() = Elevator.stop()
 
@@ -38,20 +27,34 @@ abstract class SendToTarget(private val target: Int, requireSubsystem: Boolean) 
     }
 }
 
-class SendToBottom(
-        requireSubsystem: Boolean = true
-) : SendToTarget(Elevator.BOTTOM_TARGET, requireSubsystem) {
+class SendToBottom(requireSubsystem: Boolean = true) : SendToTarget(requireSubsystem) {
     override val isAtTarget get() = Elevator.atBottom
+
+    override fun execute(): Boolean {
+        Elevator.set(downSpeed())
+        return super.execute()
+    }
 }
 
-class SendToSwitch(
-        requireSubsystem: Boolean = true
-) : SendToTarget(Elevator.SWITCH_TARGET, requireSubsystem) {
+class SendToSwitch(requireSubsystem: Boolean = true) : SendToTarget(requireSubsystem) {
     override val isAtTarget get() = Elevator.atSwitch
+    private var wasBelowTarget: Boolean by Delegates.notNull()
+
+    override fun onCreate() {
+        wasBelowTarget = Elevator.position <= Elevator.SWITCH_TARGET
+    }
+
+    override fun execute(): Boolean {
+        Elevator.set(if (wasBelowTarget) 1.0 else downSpeed())
+        return super.execute()
+    }
 }
 
-class SendToScale(
-        requireSubsystem: Boolean = true
-) : SendToTarget(Elevator.SCALE_TARGET, requireSubsystem) {
+class SendToScale(requireSubsystem: Boolean = true) : SendToTarget(requireSubsystem) {
     override val isAtTarget get() = Elevator.atTop
+
+    override fun execute(): Boolean {
+        Elevator.set(1.0)
+        return super.execute()
+    }
 }
